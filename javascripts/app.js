c4eeae07263551d31af8a8f7103ab117c6cb29ec
Hieu <mr.hieund@home.com>
2016-06-19 22:27:03 +0800

    change app.js

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,17 +1,9 @@
-var product_template = document.getElementById('product-template');
-data = fetch('data.json').then(function(response) {
-    return response.json().then(function(json) {
-        var catalog = document.getElementById('catalog');
-        json.forEach(function(data){
-            htmlText = parse(product_template.text, data);
-            catalog.insertAdjacentHTML('beforeend', htmlText);    
-        });
-    })
-}).catch(function(error) { console.log(error);});
+(function() {
+  'use strict';
 
-parse = function(text, data) {
+var product_template = document.getElementById('product-template');
+var parse = function(text, data) {
     var newText;
-    console.log(data);
     newText = text.replace('{{image}}', data.image)
                 .replace('{{brand}}', data.brand)
                 .replace('{{alt}}', data.alt)
@@ -21,9 +13,20 @@ parse = function(text, data) {
     return newText;
 }
 
+fetch('data.json').then(function(response) {
+    return response.json().then(function(json) {
+        var catalog = document.getElementById('catalog');
+        json.forEach(function(data){
+            var htmlText = parse(product_template.text, data);
+            catalog.insertAdjacentHTML('beforeend', htmlText);    
+        });
+    })
+}).catch(function(error) { console.log(error);});
+
 if ('serviceWorker' in navigator) {
 navigator.serviceWorker
          .register('/javascripts/sw.js')
          .then(function() { console.log('Service Worker Registered') })
          .catch(function(error) { console.log('Registration failed with ' + error)});
 }
+})();
