67bdb54def30b80582708626e65b9c0631eea812
Hieu <mr.hieund@home.com>
2016-06-19 20:24:02 +0800

    initial repo

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a demo prototype for progressive web app
---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+
+if ('serviceWorker' in navigator) {
+navigator.serviceWorker
+         .register('/sw.js')
+         .then(function() { console.log('Service Worker Registered'); })
+         .catch(function(error) { console.log('Registration failed with ' + error);
+  });
+}
