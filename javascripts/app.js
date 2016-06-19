46583b1877bba4e6fb7f87966fd880081f8225b8
Hieu <mr.hieund@home.com>
2016-06-19 21:49:54 +0800

    add data.json and template

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,10 +1,25 @@
+var product_template = document.getElementById('product-template');
+data = fetch('/data.json').then(function(response){
+    var processed_product_template = parse(product_template.text, response.json);
+    var catalog = document.getElementById('catalog');
+    catalog.insertAdjacentHTML('beforeend', processed_product_template);
+    catalog.insertAdjacentHTML('beforeend', processed_product_template);
+}).catch(function(error) { console.log(error);});
 
-
+parse = function(text, data) {
+    var newText;
+    newText = text.replace('{{image}}', data.image);
+    newText = text.replace('{{brand}}', data.brand);
+    newText = text.replace('{{alt}}', data.alt);
+    newText = text.replace('{{title}}', data.title);
+    newText = text.replace('{{price}}', data.price);
+    newText = text.replace('{{name}}', data.name);
+    return newText;
+}
 
 if ('serviceWorker' in navigator) {
 navigator.serviceWorker
          .register('/javascripts/sw.js')
-         .then(function() { console.log('Service Worker Registered'); })
-         .catch(function(error) { console.log('Registration failed with ' + error);
-  });
+         .then(function() { console.log('Service Worker Registered') })
+         .catch(function(error) { console.log('Registration failed with ' + error)});
 }
