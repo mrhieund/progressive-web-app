da9571483ff1b7ab837c546b93831accd13de7f5
Hieu <mr.hieund@home.com>
2016-06-19 20:48:20 +0800

    correct sw.js path

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,7 +3,7 @@
 
 if ('serviceWorker' in navigator) {
 navigator.serviceWorker
-         .register('/sw.js')
+         .register('/javascripts/sw.js')
          .then(function() { console.log('Service Worker Registered'); })
          .catch(function(error) { console.log('Registration failed with ' + error);
   });
